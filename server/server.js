1e00c333089471c65023da9a9af9b57eb53e63d8
이정근 <junggelee@ebay.com>
2020-06-23 20:43:21 +0900

    ssss

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3001
+//const axios = require('axios')
 
 app.use(bodyParser.json());
 app.use('/api', (req, res) => res.json(
+    
     {returnCode:'0000',errorMessage:'message'},
     ));
 
